71b5b08c5e9c3f3d36c03763cdc7f3359b8d9374
antonyalexcm <antonyalexcm@gmail.com>
2019-10-07 19:29:11 +0530

    spring login started

--- LoginPage1/src/com/bridgelabz/login/ConnectorDetails.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.login;
-
-public interface ConnectorDetails 
-{
-	String url = "jdbc:mysql://localhost:3306/new_schema";
-	String dbUName = "root";
-	String dbPassword = "password";
-}
--- LoginPage1/src/com/bridgelabz/login/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.login;
-
-public interface UserDAO 
-{
-	public int insertUserIntoDatabase(Users user);
-	
-	public Users getFromDatabase(String email, String pass);
-}
